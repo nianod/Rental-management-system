9cae5ff87fa99eb7015a480ba6c08143a506156f
nianod <arnoldkk422@gmail.com>
2025-12-03 17:47:31 +0300

    added bext

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+  <div>
+    <h4 className="text-green-500">Am arnold</h4>
+  </div>
+  );
+}
