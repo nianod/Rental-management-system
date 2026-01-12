cb577ac9f640a4aacf6343f3f7d886f89bb7376a
nianod <arnoldkk422@gmail.com>
2026-01-12 12:28:04 +0300

    tenant progress

---
+++ app/tenant/discussions/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <p>Raise issues affecting you here <br />
+        Anyone can see your issues including your fellow tenants
+        </p></div>
+  )
+}
+
+export default page
---
+++ app/tenant/payments/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Track your Rent Payments here</div>
+  )
+}
+
+export default page
