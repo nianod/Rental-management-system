8b72e29455e674fb11ba562a24b8585783bf0d7e
nianod <arnoldkk422@gmail.com>
2026-03-22 01:09:53 +0300

    landlord to interact with the data

--- app/tenant/updates/page.tsx
+++ app/tenant/updates/page.tsx
@@ -14,7 +14,7 @@ const Updates = () => {
         cache: "no-store",
       });
 
-      if (!response.ok) {
+      if (!response.ok) { 
         throw new Error("Failed to fetch updates");
       }
 
